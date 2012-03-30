c179811678e347c6450f83d1e70011c9c1dc3f43
Dave Doolin <david.doolin@gmail.com>
2012-03-30 10:46:27 -0700

    worked on script for checking module presence

--- test/environment.py
+++ test/environment.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 
-import datetime, copy, csv, igraph, math, os, random, re, sqlite3
+print "testing for imports"
+
+import datetime, copy, csv
+import math, os, random, re, sqlite3
 import sys, time, types, unicodedata
 from xml.dom  import minidom
 import zipfile
+
+imports = ["foo", "bar"]
+
+try:
+    import zipfile
+    print "zipfile found"
+except ImportError:
+    print "zipfile not found"
+
+
+try:
+    import igraph
+    print "igraph found"
+except ImportError:
+    print "igraph not found"
+
