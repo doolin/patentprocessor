18ccb3b246d5bf00f3a71ae689f33bc3e2b2e0a9
Dave Doolin <david.doolin@gmail.com>
2012-04-04 18:48:32 -0400

    finally had to start renaming stuff.

--- XMLParse2008.py
+++ XMLParse2008.py
@@ -13,7 +13,7 @@ files = [x for x in os.listdir(flder)
          if re.match(r"ipg\d{6}.one.xml", x, re.I)!=None]
 print "Total files: %d" % (len(files))
 
-tblList = ["assignee", "citation", "class", "inventor", "patent", "patdesc", "lawyer", "sciref", "usreldoc"]
+tables = ["assignee", "citation", "class", "inventor", "patent", "patdesc", "lawyer", "sciref", "usreldoc"]
 for filenum, filename in enumerate(files):    
     print " > Regular Expression: %s" % filename
     XMLs = re.findall(
@@ -33,10 +33,10 @@ for filenum, filename in enumerate(files):
             print "  - Error: %s (%d)  %s" % (filename, i, x[175:200])
     print "   - number of patents:", len(xmllist), datetime.datetime.now()-t1
 
-    for x in tblList:
-        SQLPatent().dbBuild(q=SQLPatent().tblBuild(xmllist, tbl=x), tbl=x, week=filename)
+    for table in tables:
+        SQLPatent().dbBuild(q=SQLPatent().tblBuild(xmllist, tbl=table), tbl=table, week=filename)
     print "   -", datetime.datetime.now()-t1
 
 
-for x in tblList:
-    SQLPatent().dbFinal(tbl=x)
+for table in tables:
+    SQLPatent().dbFinal(tbl=table)
