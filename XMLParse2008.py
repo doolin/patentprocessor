6251d349f14de331a86f1285d7477b6c5661ada9
Dave Doolin <david.doolin@gmail.com>
2012-04-27 23:46:05 -0400

    configured to parse 2011 data

--- XMLParse2008.py
+++ XMLParse2008.py
@@ -1,17 +1,22 @@
+import sys
 from patXML import SQLPatent
 from patXML import XMLPatent
 from patXML import uniasc
 from fwork  import *
 import os, datetime, re
 
-flder='test'
+#flder='test'
+flder = '../../patents/2011'
 t1 = datetime.datetime.now()
 
 #get a listing of all files within the directory that follow the naming pattern
 files = [x for x in os.listdir(flder)
          #if re.match(r"ip[a-z]{2}[0-9]{6,8}[.]xml", x, re.I)!=None]
-         if re.match(r"ipg\d{6}.one.xml", x, re.I)!=None]
+         #if re.match(r"ipg\d{6}.one.xml", x, re.I)!=None]
+         if re.match(r"ipg\d{6}.xml", x, re.I)!=None]
 print "Total files: %d" % (len(files))
+
+#sys.exit()
 
 tables = ["assignee", "citation", "class", "inventor", "patent", "patdesc", "lawyer", "sciref", "usreldoc"]
 for filenum, filename in enumerate(files):    
