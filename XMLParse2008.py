d7e064c7728e22cde6fa02c99daaf16c03d7f3f9
Dave Doolin <david.doolin@gmail.com>
2012-04-30 12:24:36 -0700

    Added error counting to xml parsing

--- XMLParse2008.py
+++ XMLParse2008.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append( '.' )
 from patXML import SQLPatent
 from patXML import XMLPatent
 from patXML import uniasc
@@ -10,7 +12,7 @@ t1 = datetime.datetime.now()
 #get a listing of all files within the directory that follow the naming pattern
 files = [x for x in os.listdir(flder)
          #if re.match(r"ip[a-z]{2}[0-9]{6,8}[.]xml", x, re.I)!=None]
-         if re.match(r"ipg\d{6}.one.xml", x, re.I)!=None]
+         if re.match(r"pg030520.xml", x, re.I)!=None]
 print "Total files: %d" % (len(files))
 
 tables = ["assignee", "citation", "class", "inventor", "patent", "patdesc", "lawyer", "sciref", "usreldoc"]
@@ -26,13 +28,16 @@ for filenum, filename in enumerate(files):
     print "   - Total Patents: %d" % (len(XMLs))
 
     xmllist = []
+    count = 0
     for i, x in enumerate(XMLs):
         try:
             xmllist.append(XMLPatent(x))
-        except:
+        except Exception as inst:
+            print type(inst)
             print "  - Error: %s (%d)  %s" % (filename, i, x[175:200])
+            count += 1
     print "   - number of patents:", len(xmllist), datetime.datetime.now()-t1
-
+    print "   - number of errors: " ,count
     for table in tables:
         SQLPatent().dbBuild(q=SQLPatent().tblBuild(xmllist, tbl=table), tbl=table, week=filename)
     print "   -", datetime.datetime.now()-t1
