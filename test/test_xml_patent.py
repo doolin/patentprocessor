badb9798689519fdc82816e7de174196c2df94b3
Alvin Chang <alvinchang@berkeley.edu>
2012-05-25 22:27:21 -0700

    Just moving stuff around, basically just testing small commits.

--- test/test_xml_patent.py
+++ test/test_xml_patent.py
@@ -10,29 +10,29 @@ from xml_patent import XMLPatent
 
 dir = os.path.dirname(__file__) # Store directory of file 
 test_folder = os.path.join(dir, 'xml/unittest') # Folder path with xml unit test files
-log_folder = os.path.join(dir, 'xml/unittest/xml-parsing-log.txt') # Folder path unit test log
+log_folder = os.path.join(dir, 'xml/unittest/test-log.txt') # Folder path unit test log
 
 open(logfile, 'w') # Erase the log and start over for each run (just a test)
 logging.basicConfig(filename=logfile, level=logging.DEBUG) # Set up log in case its needed
 
 
-t1 = datetime.datetime.now() #set up time in case it will be used
 
-files = [x for x in os.listdir(test_folder)
-         if re.match(r"unit-test.xml", x, re.I)!=None] # list collection of files to be used for unit-testing
 
-xml_file = open(('xml/ipg120327.one.xml'), 'U') #example file
+#xml_file = open(('xml/ipg120327.one.xml'), 'U') #example file
 
 
 class TestXMLPatent(unittest.TestCase):
 
     def setUp(self):
+        t1 = datetime.datetime.now() #set up time in case it will be used
+        files = [x for x in os.listdir(test_folder) if re.match(r"unit-test.xml", x, re.I)!=None] # list collection of files to be used for unit-testing
         
 
     def test_dummy(self):
         assert(1 == 1)
 
-    def test_patent(self):
+    def test_patents(self):
+        for 
 
         
 
