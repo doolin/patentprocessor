fb0fa72795c431d468fe546dc123985723dd074a
Alvin Chang <alvinchang@berkeley.edu>
2012-05-30 10:35:45 -0700

    Fixing more minor formatting issues

--- test/test_xml_patent.py
+++ test/test_xml_patent.py
@@ -26,6 +26,13 @@ debug = False
 xml_files = []
 parsed_xml = []
 
+# TODO:
+#Fix formatting
+#Update field logic tests
+#Update xml presence tests
+#Implement logging
+#Profiling?
+
 
 """
  Fields useful for legacy code testing: self.country, self.patent, self.kind,
@@ -70,32 +77,36 @@ class TestXMLPatent(unittest.TestCase):
         if debug:
             print "\n     Testing Logic and Format of Patent Fields\n"
         for i, xml_tuple in enumerate(parsed_xml): 
-            parsed_fields = xml_tuple[1]                                                             # Note that str.isalum()/isdigit() intrinsically check for non-emptyness
-            self.assertTrue(parsed_fields.pat_type.isalnum())                                        # Testing legitimate patent type, e.g. design, scientific, etc... is alphanumeric
-            self.assertTrue(parsed_fields.patent.isalnum())                                          # Testing patent , e.g. D0656296
-            self.assertTrue(parsed_fields.date_grant.isdigit() and len(parsed_fields.date_app) is 8) # Testing date/format of grant, e.g. 20123456
-            self.assertTrue(parsed_fields.date_app.isdigit() and len(parsed_fields.date_grant) is 8) # Testing date/format of grant application date, e.g. 20124567
-            self.assertTrue(parsed_fields.country.isalnum())                                         # Similar testing for the following....
+            parsed_fields = xml_tuple[1]                                                             
+            self.assertTrue(parsed_fields.pat_type.isalnum())                                       
+            self.assertTrue(parsed_fields.patent.isalnum())                                          
+            self.assertTrue(parsed_fields.date_grant.isdigit() andlen(parsed_fields.date_app) is 8)
+            self.assertTrue(parsed_fields.date_app.isdigit() and len(parsed_fields.date_grant) is 8) 
+            self.assertTrue(parsed_fields.country.isalnum())                                         
             self.assertTrue(parsed_fields.country_app.isalnum())
             self.assertTrue(parsed_fields.kind.isalnum())
             if debug:
                 print " - Testing Patent: %d ..... Passed!" %(i+1)
 
-    def test_patent_validity(self): # Low-level test, testing presence of fields in original XML, thinking of compiling regex's but no point as each pattern only used once.
+    def test_patent_validity(self): # Low-level test, testing presence of fields in original XML
         if debug:
             print "\n     Testing XML Presence of Patent Fields\n"
         for i, xml_tuple in enumerate(parsed_xml): # xml_tuple = (file of xml, XMLpatent(xml))
             original_xml_string = open(xml_tuple[0]).read()
             parsed_fields = xml_tuple[1]
-            country_match = re.search(r"[<]country[>]"+parsed_fields.country+"[<][/]country[>]", original_xml_string, re.I + re.S + re.X)
+            country_match = re.search(r"[<]country[>]"+parsed_fields.country+"[<][/]country[>]",
+                                      original_xml_string, re.I + re.S + re.X)
             self.assertTrue(country_match)
-            kind_match = re.search(r"[<]kind[>]"+parsed_fields.kind+"[<][/]kind[>]", original_xml_string, re.I + re.S + re.X)
+            kind_match = re.search(r"[<]kind[>]"+parsed_fields.kind+"[<][/]kind[>]", original_xml_string,
+                                   re.I + re.S + re.X)
             self.assertTrue(kind_match)
 
-            #still working on this one and others, may have annoying \n, whitespace in middle, need to use rstrip, lstrip
+            #still working on this one and others, may have annoying \n, whitespace in middle
+            #need to use rstrip, lstrip
 
             #print "[>]"+parsed_fields.invention_title+"[<][/]invention-title[>]"
-            #invention_title_match = re.search("[>]"+parsed_fields.invention_title+"[<][/]invention-title[>]", original_xml_string, re.I + re.S + re.X)
+            #invention_title_match = re.search("[>]"+parsed_fields.invention_title+"[<][/]invention-title[>]",
+            #                                  original_xml_string, re.I + re.S + re.X)
             #self.assertTrue(invention_title_match)
 
             if debug:
@@ -107,7 +118,7 @@ class TestXMLPatent(unittest.TestCase):
 
 if __name__ == '__main__':
 
-    parser = OptionParser() #Set up Options parser to have a debugging flag, future folder/dirs to store logging can be added here as well
+    parser = OptionParser()
     parser.add_option("-d", "--debugging", dest="debugging", action="store_true")
     (options, args) = parser.parse_args()
 
