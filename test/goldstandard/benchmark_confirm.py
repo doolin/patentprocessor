337a6782cc133ae92c1d4f068ddda4fcdeea9fd8
Alvin Chang <alvinchang@berkeley.edu>
2012-07-02 15:11:57 -0700

    Adding logging

--- test/goldstandard/benchmark_confirm.py
+++ test/goldstandard/benchmark_confirm.py
@@ -4,12 +4,16 @@ import sys
 import logging
 import benchmark
 
-
-
 #  bmVerify(['final_r7', 'final_r8'], filepath="/home/ysun/disambig/newcode/all/", outdir = "/home/ayu/results_v2/")
         
-
 # Text Files
 txt_file = 'benchmark_errors.txt'
 opened_file = open(txt_file, 'U')
 log_file = 'benchmark_results.log'
+
+# Logging
+logging.basicConfig(filename=log_file, level=logging.DEBUG)
+open(log_file, "w")
+
+
+
