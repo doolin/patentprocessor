f10b2389bb8fde9e44855791aa5c8afb89b781fd
Dave Doolin <david.doolin@gmail.com>
2012-07-02 18:10:48 -0700

    writing out new benchmark script

--- test/goldstandard/benchmark.sh
+++ test/goldstandard/benchmark.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 
-# gawk -F, '{print $2",",$3",",$4}' < benchmark.csv
-gawk -F, '{print $2}' < benchmark.csv > patentlist.txt
+gawk -F, '{print $1",", $2",",$3",",$4}' < benchmark.csv > dt5.csv
+
+#gawk -F, '{print $2}' < benchmark.csv > patentlist.txt
 
