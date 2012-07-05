7a65115459db284580ae39aea5e7e1cf23eb09aa
Alvin Chang <alvinchang@berkeley.edu>
2012-07-05 11:34:52 -0700

    Adding initialize methods

--- generator.py
+++ generator.py
@@ -10,4 +10,19 @@ import logging
 # Make this better, refactor, test
 # Remove precision, density, recall
 
+def initialize_sql_databases():
+    # Set Up SQL Connections
+    # Database to connect to
+    con = sql.connect('invpat.sqlite3')
+    # Database to write to 
+    fin = sql.connect('mercury2.sqlite3') 
+
+def initialize_logging(log_file):
+    # Logging
+    logging.basicConfig(filename=log_file, level=logging.DEBUG)
+    open(log_file, "w")
+    count = 0
+    success = 0
+    errors = 0
+
 
