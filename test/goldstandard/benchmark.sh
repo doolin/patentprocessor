0d1fd81237d00ae2f6ab76bfdab2a46b7011871c
Alvin Chang <alvinchang@berkeley.edu>
2012-07-05 14:33:30 -0700

    added benchmark.sh

--- test/goldstandard/benchmark.sh
+++ test/goldstandard/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-# gawk -F, '{print $2",",$3",",$4}' < benchmark.csv
-gawk -F, '{print $2}' < benchmark.csv > patentlist.txt
+gawk -F, '{print $2",",$3",",$4}' < benchmark.csv > patentlist.txt
+#gawk -F, '{print $2}' < benchmark.csv > patentlist.txt
 
