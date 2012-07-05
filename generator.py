c764c3d6ebb6b2a8c57ff42574e983b2f176501c
Alvin Chang <alvinchang@berkeley.edu>
2012-07-05 11:31:03 -0700

    Starting generator.py framework, added todo

---
+++ generator.py
@@ -0,0 +1,13 @@
+import sqlite3 as sql
+import sys
+import os
+import logging
+
+
+# TODO: 
+# Need to verify correctness
+# Use command-line args to pass in file names
+# Make this better, refactor, test
+# Remove precision, density, recall
+
+
