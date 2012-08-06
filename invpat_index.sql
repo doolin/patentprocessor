759d633d68f66021492a89435b9e6195a334c66c
Dave Doolin <david.doolin@gmail.com>
2012-08-06 13:44:55 -0700

    removed upper and lower indexes

--- invpat_index.sql
+++ invpat_index.sql
@@ -3,7 +3,5 @@ CREATE INDEX idx_firstlastname on invpat ( firstname, lastname );
 CREATE INDEX idx_firstname on invpat(firstname);
 CREATE UNIQUE INDEX idx_invnum on invpat (Invnum);
 CREATE INDEX idx_lastname on invpat(lastname);
-CREATE INDEX idx_lower on invpat(lower);
 CREATE INDEX idx_patent on invpat(patent);
-CREATE INDEX idx_upper on invpat(upper);
 CREATE UNIQUE INDEX index_invnum_on_invpat ON invpat(invnum);
