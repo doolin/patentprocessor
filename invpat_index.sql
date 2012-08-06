aa82946bb4dd66cf35efa246565af6d7bd45f002
Dave Doolin <david.doolin@gmail.com>
2012-08-06 13:46:47 -0700

    cleaned up index script

--- invpat_index.sql
+++ invpat_index.sql
@@ -1,7 +1,6 @@
-CREATE INDEX idx_assignee on invpat(assignee);
-CREATE INDEX idx_firstlastname on invpat ( firstname, lastname );
-CREATE INDEX idx_firstname on invpat(firstname);
-CREATE UNIQUE INDEX idx_invnum on invpat (Invnum);
-CREATE INDEX idx_lastname on invpat(lastname);
-CREATE INDEX idx_patent on invpat(patent);
-CREATE UNIQUE INDEX index_invnum_on_invpat ON invpat(invnum);
+CREATE INDEX idx_assignee on invpat(Assignee);
+CREATE INDEX idx_firstlastname on invpat(Firstname, Lastname );
+CREATE INDEX idx_firstname on invpat(Firstname);
+CREATE UNIQUE INDEX idx_invnum on invpat(Invnum);
+CREATE INDEX idx_lastname on invpat(Lastname);
+CREATE INDEX idx_patent on invpat(Patent);
