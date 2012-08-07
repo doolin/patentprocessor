8680b4d991e704a0fc3079f7a267a190815c413a
Dave Doolin <david.doolin@gmail.com>
2012-08-07 10:51:59 -0700

    extending migration file to manage whole nine yards. Probably take 9 hours to run. No big deal.

--- sql/invpat_1.sql
+++ sql/invpat_1.sql
@@ -79,3 +79,90 @@ SELECT
         Invnum_N,
         Unique_Record_ID
 FROM invpat;
+
+-- Now we drop the original table...
+drop table invpat;
+
+-- ...because we want to recreate it...
+CREATE TABLE invpat(
+	Firstname TEXT,
+        Middlename TEXT,
+        Lastname TEXT,
+        Street TEXT,
+        City TEXT,
+        State TEXT,
+        Country TEXT,
+        Zipcode TEXT,
+        Latitude REAL,
+        Longitude REAL,
+        InvSeq INT,
+        Patent TEXT,
+        AppYear TEXT,
+        ApplyYear TEXT,
+        GYear INT,
+        AppDate TEXT,
+        Assignee TEXT,
+        AsgNum INT,
+        Class TEXT,
+        Coauthor TEXT,
+        Invnum TEXT,
+        Invnum_N TEXT,
+        Unique_Record_ID TEXT);
+
+-- Now, let's repopulate the original table with data conforming
+-- to the same schema as the patent preprocessor emits.
+INSERT INTO invpat(
+        Firstname,
+        Middlename,
+        Lastname,
+        Street,
+        City,
+        State,
+        Country,
+        Zipcode,
+        Latitude,
+        Longitude,
+        InvSeq,
+        Patent,
+        AppYear,
+        ApplyYear,
+        GYear,
+        AppDate,
+        Assignee,
+        AsgNum,
+        Class,
+        Coauthor,
+        Invnum,
+        Invnum_N,
+        Unique_Record_ID)
+SELECT 
+        Firstname,
+        Middlename,
+        Lastname,
+        Street,
+        City,
+        State,
+        Country,
+        Zipcode,
+        Latitude,
+        Longitude,
+        InvSeq,
+        Patent,
+        AppYear,
+        ApplyYear,
+        GYear,
+        AppDate,
+        Assignee,
+        AsgNum,
+        Class,
+        Coauthor,
+        Invnum,
+        Invnum_N,
+        Unique_Record_ID
+FROM invpat_1;
+
+-- Now we drop all the current indexes... 
+
+-- Add new indexes conforming to preprocessing schema...
+
+
