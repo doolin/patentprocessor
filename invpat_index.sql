aae5a8e6d44c0e3010904f3fc901c61cd2f1c2a8
Dave Doolin <david.doolin@gmail.com>
2012-08-06 21:26:22 -0700

    added correct index

--- invpat_index.sql
+++ invpat_index.sql
@@ -1,9 +1,9 @@
-CREATE INDEX idx_assignee on invpat(assignee);
-CREATE INDEX idx_firstlastname on invpat ( firstname, lastname );
-CREATE INDEX idx_firstname on invpat(firstname);
-CREATE UNIQUE INDEX idx_invnum on invpat (Invnum);
-CREATE INDEX idx_lastname on invpat(lastname);
-CREATE INDEX idx_lower on invpat(lower);
-CREATE INDEX idx_patent on invpat(patent);
-CREATE INDEX idx_upper on invpat(upper);
-CREATE UNIQUE INDEX index_invnum_on_invpat ON invpat(invnum);
+CREATE INDEX asg on invpat (Assignee);
+CREATE INDEX asg2 on invpat (AsgNum);
+CREATE INDEX gyr on invpat (Gyear);
+CREATE INDEX iNidx  ON invpat (Invnum_N);
+CREATE INDEX locc on invpat (City);
+CREATE INDEX loccs on invpat (City, State);
+CREATE INDEX locs on invpat (State);
+CREATE INDEX pdx ON invpat (Patent);
+CREATE INDEX pidx ON invpat (Patent, InvSeq);
