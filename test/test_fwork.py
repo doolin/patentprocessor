f83ef76e89977bab606b63384865e78650968453
Dave Doolin <david.doolin@gmail.com>
2012-08-18 19:13:44 -0700

    started adding test code to quickSQL

--- test/test_fwork.py
+++ test/test_fwork.py
@@ -39,6 +39,37 @@ class TestFWork(unittest.TestCase):
     def test_remove_period(self):
         assert('10' == ascit('1.0', strict=True))
 
+#    def test_quickSQL(self):
+#        import sqlite3
+#	self.conn = sqlite3.connect(":memory:")
+#	self.cursor = self.conn.cursor()
+#	data = [ ["Unique_ID", "Name"], [1, 1], [2, 2], [3, 3], [4, 4] ]
+#	quickSQL(self.cursor, data, table="test")
+#	self.conn.close
+
+
+    def test_get_ctypes(self):
+        assert('VARCHAR' == get_ctypes("FOO"))
+        assert('REAL'    == get_ctypes(4.2))
+        assert('INTEGER' == get_ctypes(42))
+
+    def get_quicksql_data(self):
+	return [
+		[u'UniqueID', u'Patent', u'Lastname', u'Firstname'],
+		[u'1', u'08194655', u'PISTER', u'KRISTOPHER S J'],
+		[u'1', u'08190055', u'PISTER', u'KRISTOPHER S J']
+               ]
+
+    def test_quickSQL2(self):
+        import sqlite3
+	dbfilename = "fwork.sqlite3"
+	self.removeFile(dbfilename)
+	self.conn = sqlite3.connect(dbfilename)
+	self.cursor = self.conn.cursor()
+	data = self.get_quicksql_data()
+	quickSQL2(self.cursor, data, table="test", header=True, typeList=[u'Patent VARCHAR'])
+	self.conn.commit()
+	self.cursor.close()
 
 if __name__ == '__main__':
     unittest.main()
