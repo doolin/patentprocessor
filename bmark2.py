137ae31bcab1d6aae07b96f18b7fd095dba8fa0d
Dave Doolin <david.doolin@gmail.com>
2012-08-20 09:06:09 -0700

    refactored handling fuzzy dataS

--- bmark2.py
+++ bmark2.py
@@ -56,6 +56,35 @@ def create_match_tables(c, fBnme, uqB, exCom, exAnd):
 
 	    """ % (fBnme, uqB, exCom, exAnd, uqB, exCom))
 
+def handle_fuzzy_dataS(c, exCom, uqB, uqS, fuzzy, fBnme, exAnd):
+	    c.executescript("""
+		CREATE INDEX IF NOT EXISTS dS_E ON dataS (%s);
+
+		/* RETAIN ONLY JARO>0.9 FUZZY AND EXACT MATCHES */
+		CREATE TABLE dataM AS
+		    SELECT  a.*, %s AS uqB, %s AS uqS, %s AS jaro
+		      FROM  %s AS a
+		INNER JOIN  dataS AS b
+			ON  %s
+		     WHERE  jaro>0.90;
+
+		/* DETERMINE MAXIMUM JARO FOR EACH UQ AND EXACT COMBO */
+		CREATE TABLE dataT AS
+		    SELECT  uqS, %s, MAX(jaro) AS jaro, count(*) as cnt
+		      FROM  dataM
+		  GROUP BY  uqS, %s;
+
+		/* RETAIN ONLY MAXIMUM JARO */
+		CREATE TABLE dataM2 AS
+		    SELECT  a.*
+		      FROM  dataM AS a
+		INNER JOIN  dataT AS b
+			ON  a.uqS=b.uqS AND a.jaro=b.jaro AND %s;
+		""" % (exCom, uqB, uqS, 
+		       "*".join(["jarow(a.%s, b.%s)" % (x,x) for x in fuzzy]),
+		       fBnme, exAnd, exCom, exCom, exAnd))
+
+
 
 def bmVerify(results, filepath="", outdir = ""):
         """
@@ -171,34 +200,36 @@ def bmVerify(results, filepath="", outdir = ""):
 
                 if fuzzy:
                     c.execute("CREATE INDEX IF NOT EXISTS dS_E ON dataS (%s);" % (exCom))
-                    c.executescript("""
-                        CREATE INDEX IF NOT EXISTS dS_E ON dataS (%s);
-
-                        /* RETAIN ONLY JARO>0.9 FUZZY AND EXACT MATCHES */
-                        CREATE TABLE dataM AS
-                            SELECT  a.*, %s AS uqB, %s AS uqS, %s AS jaro
-                              FROM  %s AS a
-                        INNER JOIN  dataS AS b
-                                ON  %s
-                             WHERE  jaro>0.90;
-
-                        /* DETERMINE MAXIMUM JARO FOR EACH UQ AND EXACT COMBO */
-                        CREATE TABLE dataT AS
-                            SELECT  uqS, %s, MAX(jaro) AS jaro, count(*) as cnt
-                              FROM  dataM
-                          GROUP BY  uqS, %s;
-
-                        /* RETAIN ONLY MAXIMUM JARO */
-                        CREATE TABLE dataM2 AS
-                            SELECT  a.*
-                              FROM  dataM AS a
-                        INNER JOIN  dataT AS b
-                                ON  a.uqS=b.uqS AND a.jaro=b.jaro AND %s;
-                        """ % (exCom, uqB, uqS, 
-                               "*".join(["jarow(a.%s, b.%s)" % (x,x) for x in fuzzy]),
-                               fBnme, exAnd, exCom, exCom, exAnd))
+                    handle_fuzzy_dataS(c, exCom, uqB, uqS, fuzzy, fBnme, exAnd)
+#                    c.executescript("""
+#                        CREATE INDEX IF NOT EXISTS dS_E ON dataS (%s);
+#
+#                        /* RETAIN ONLY JARO>0.9 FUZZY AND EXACT MATCHES */
+#                        CREATE TABLE dataM AS
+#                            SELECT  a.*, %s AS uqB, %s AS uqS, %s AS jaro
+#                              FROM  %s AS a
+#                        INNER JOIN  dataS AS b
+#                                ON  %s
+#                             WHERE  jaro>0.90;
+#
+#                        /* DETERMINE MAXIMUM JARO FOR EACH UQ AND EXACT COMBO */
+#                        CREATE TABLE dataT AS
+#                            SELECT  uqS, %s, MAX(jaro) AS jaro, count(*) as cnt
+#                              FROM  dataM
+#                          GROUP BY  uqS, %s;
+#
+#                        /* RETAIN ONLY MAXIMUM JARO */
+#                        CREATE TABLE dataM2 AS
+#                            SELECT  a.*
+#                              FROM  dataM AS a
+#                        INNER JOIN  dataT AS b
+#                                ON  a.uqS=b.uqS AND a.jaro=b.jaro AND %s;
+#                        """ % (exCom, uqB, uqS, 
+#                               "*".join(["jarow(a.%s, b.%s)" % (x,x) for x in fuzzy]),
+#                               fBnme, exAnd, exCom, exCom, exAnd))
 
                 else:
+		    # TODO: Refactor into handle_dataS()
                     c.executescript("""
                         CREATE INDEX IF NOT EXISTS dS_E ON dataS (%s);
                         CREATE TABLE dataM2 AS
