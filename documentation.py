912ef4653d868c34f24576c7b39d9fdb3b1498f0
Kevin Shen <kevshin222@hotmail.com>
2012-09-10 13:34:08 -0700

    updated testing file

---
+++ documentation.py
@@ -0,0 +1,29 @@
+"""
+Documentation
+Patentprocessor testing file
+"""
+
+def inventor(self):
+    """Documentation for a function.
+    A list with a collection of inventer's name
+    """
+    return
+
+def func(self):
+    """Documentation for a function.
+    More details.
+    """
+    pass
+
+def patents(self):
+    """Documentation for a function.
+    A list with a collection of patents that belong to each inventor.
+    """
+    return
+
+def assignee(self):
+    """List of assignees.
+
+    """
+    return
+    
