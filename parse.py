6fe0bb5a6d2c769f189277633e2f34f89f113703
Gabe Fierro <gt.fierro@berkeley.edu>
2012-10-15 19:41:21 -0700

    use parallel regex instead of serial

--- parse.py
+++ parse.py
@@ -104,28 +104,13 @@ print "Total files: %d" % (len(files))
 logging.info("Total files: %d" % (len(files)))
 
 # list of parsed xml strings
-<<<<<<< HEAD
 parsed_xmls = parallel_parse(files)
 logging.info("   - Total Patents: %d" % (len(parsed_xmls)))
-=======
-parsed_xmls = []
->>>>>>> add framework for parallel parsing
 
 tables = ["assignee", "citation", "class", "inventor", "patent",\
         "patdesc", "lawyer", "sciref", "usreldoc"]
 total_count = 0
 total_patents = 0
-for filenum, filename in enumerate(files):
-    print " > Regular Expression: %s" % filename
-    XMLs = re.findall(
-            r"""
-                ([<][?]xml[ ]version.*?[>]       #all XML starts with ?xml
-                .*?
-                [<][/]us[-]patent[-]grant[>])    #and here is the end tag
-             """,
-            open(files[filenum]).read(), re.I + re.S + re.X)
-    print "   - Total Patents: %d" % (len(XMLs))
-    logging.info("   - Total Patents: %d" % (len(XMLs)))
 
 for filename in parsed_xmls:
 
@@ -135,7 +120,7 @@ for filename in parsed_xmls:
 
     for i, x in enumerate(parsed_xmls):
         try:
-            xmllist.append(XMLPatent(x))
+            xmllist.append(XMLPatent(x))   #TODO: this is the slow part (parallelize)
             patents += 1
         except Exception as inst:
             logging.error(type(inst))
