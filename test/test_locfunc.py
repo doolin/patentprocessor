b39127783193e6da4285ca9979cf5e96718f5c4e
Dave Doolin <david.doolin@gmail.com>
2012-10-17 13:54:47 -0700

    first unit test on US for locfunc, St Claire

--- test/test_locfunc.py
+++ test/test_locfunc.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+# The main purpose of these tests is watching
+# the general behavior of the cityctry function.
+# Not every case needs to be tested, but at least
+# a few cases should be checked. Also, at the time
+# of this writing, there was a bug in the location
+# handling code, so hitting these unit tests helps
+# ensure that the bug wasn't part of that problem.
+
+#cityctry(city, ctry, ret="city")
+
 # `locfunc.py` will probably be renamed to something a little
 # more suggestive as to its purpose.
 
@@ -19,8 +29,10 @@ class TestLocFunc(unittest.TestCase):
     def setUp(self):
         self.foo = 'bar'
 
-    def test_dummy(self):
-        assert(1 == 1)
+    def test_us(self):
+        city = cityctry("St Claire", "US")
+        print city
+        assert("SAINT CLAIRE" == city)
 
 
 if __name__ == '__main__':
