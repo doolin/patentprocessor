ae5d4b3c02f4112ce0adbd2f3025fef5c7f2560f
Alvin Chang <alvinchang@berkeley.edu>
2012-11-11 16:09:07 -0800

    adding framework for claims unit-testing...

---
+++ claims/test_claims_driver.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import unittest
+import re
+import os
+import sys
+import logging
+
+class TestClaimDriver(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
