d424c8de6935f7b92aca6182c8bd35969307bd7f
Alvin Chang <alvinchang@berkeley.edu>
2012-11-11 16:43:22 -0800

    adding basic unit-test sanity check

--- claims/test_claims_driver.py
+++ claims/test_claims_driver.py
@@ -5,11 +5,27 @@ import re
 import os
 import sys
 import logging
+from claim_driver import *
+
+# Have 3 test files, with one, ten, hundred patents.
+
+test_patent_one = "pg020507one.xml"
+test_patent_ten = "pg020507ten.xml"
+test_patent_hundred = "pg020507hundred.xml"
+
 
 class TestClaimDriver(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.c = Claims()
+        self.claim_list = []
+
+    def test_claims_init(self):
+        assert(not self.c.XMLs)
+
+        
+
+
 
 
 if __name__ == '__main__':
