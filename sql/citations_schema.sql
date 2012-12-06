0e7fd9f70f14f1d7cd47d6cfa17ba40ca111f414
Dave Doolin <david.doolin@gmail.com>
2012-12-05 17:30:52 -0800

    cleaned up formatting for easier reading

--- sql/citations_schema.sql
+++ sql/citations_schema.sql
@@ -5,14 +5,14 @@
  * are identical.
  */
 CREATE TABLE citation00_10 (
-  Patent VARCHAR(8),
-  Cit_Date INTEGER,
-  Cit_Name VARCHAR(10),
-  Cit_Kind VARCHAR(1),
+  Patent      VARCHAR(8),
+  Cit_Date    INTEGER,
+  Cit_Name    VARCHAR(10),
+  Cit_Kind    VARCHAR(1),
   Cit_Country VARCHAR(2),
-  Citation VARCHAR(8),
-  Category VARCHAR(15),
-  CitSeq INTEGER);
+  Citation    VARCHAR(8),
+  Category    VARCHAR(15),
+  CitSeq      INTEGER);
 
 
 CREATE UNIQUE INDEX idx_idx1 ON citation00_10 (patent,citation);
