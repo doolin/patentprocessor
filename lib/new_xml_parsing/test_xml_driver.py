b4e527747904f72e4f65f77a0f5df99bfde8d994
Gabe Fierro <gtfierro225@gmail.com>
2013-01-07 11:30:23 -0800

    add unittest skeleton for xml_driver

---
+++ lib/new_xml_parsing/test_xml_driver.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import re
+import unittest
+from xml_driver import XMLElement, XMLHandler
+from xml.sax import make_parser, handler
+
+# Directory of test files
+xml_files = [x for x in os.listdir('test_xml_files')
+             if re.match(r"2012_\d.xml", x) != None] # Match fixtures
+
+parsed_xml = []
+for xf in xml_files:
+    parser = make_parser()
+    xmlhandler = XMLHandler()
+    parser.setContentHandler(xmlhandler)
+    parser.setFeature(handler.feature_external_ges, False)
+    parser.parse('test_xml_files/'+xf)
+    parsed_xml.append(xmlhandler.root)
+
+class Test_XMLElement(unittest.TestCase):
+    
+    def setUp(self):
+        # sanity check
+        self.assertTrue(xml_files)
+
