aab1d92fdd8f2b25dd4b85fa0995c38a44227500
Gabe Fierro <gtfierro225@gmail.com>
2013-01-14 17:52:44 -0800

    fix incorrect loop, parse.py should behave now

--- parse.py
+++ parse.py
@@ -46,91 +46,93 @@ def parse_file(filename):
 
 def parallel_parse(filelist):
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    return itertools.chain(*pool.imap_unordered(parse_file, filelist))
+    parsed = pool.imap_unordered(parse_file, filelist)
+    return list(parsed)
 
+#TODO: pull out modular functionality into unittest-able methods
+if __name__ == '__main__':
 
-# setup argparse
-parser = argparse.ArgumentParser(description=\
-        'Specify source directory/directories for xml files to be parsed')
-parser.add_argument('--directory','-d', type=str, nargs='+', default='',
-        help='comma separated list of directories relative to $PATENTROOT that \
-        parse.py will search for .xml files')
-parser.add_argument('--patentroot','-p', type=str, nargs='?',
-        default=os.environ['PATENTROOT'] \
-        if os.environ.has_key('PATENTROOT') else '/',
-        help='root directory of all patent files/directories')
-parser.add_argument('--xmlregex','-x', type=str, 
-        nargs='?', default=r"ipg\d{6}.xml",
-        help='regex used to match xml files in each directory')
-parser.add_argument('--verbosity', '-v', type = int,
-        nargs='?', default=0,
-        help='Set the level of verbosity for the computation. The higher the \
-        verbosity level, the less restrictive the print policy. 0 (default) \
-        = error, 1 = warning, 2 = info, 3 = debug')
+    # setup argparse
+    parser = argparse.ArgumentParser(description=\
+            'Specify source directory/directories for xml files to be parsed')
+    parser.add_argument('--directory','-d', type=str, nargs='+', default='',
+            help='comma separated list of directories relative to $PATENTROOT that \
+            parse.py will search for .xml files')
+    parser.add_argument('--patentroot','-p', type=str, nargs='?',
+            default=os.environ['PATENTROOT'] \
+            if os.environ.has_key('PATENTROOT') else '/',
+            help='root directory of all patent files/directories')
+    parser.add_argument('--xmlregex','-x', type=str, 
+            nargs='?', default=r"ipg\d{6}.xml",
+            help='regex used to match xml files in each directory')
+    parser.add_argument('--verbosity', '-v', type = int,
+            nargs='?', default=0,
+            help='Set the level of verbosity for the computation. The higher the \
+            verbosity level, the less restrictive the print policy. 0 (default) \
+            = error, 1 = warning, 2 = info, 3 = debug')
 
-# double check that variables are actually set
-# we ignore the verbosity argument when determining
-# if any variables have been set by the user
-specified = [arg for arg in sys.argv if arg.startswith('-')]
-nonverbose = [opt for opt in specified if '-v' not in opt]
-if len(nonverbose) == 0:
-    parser.print_help()
-    sys.exit(1)
+    # double check that variables are actually set
+    # we ignore the verbosity argument when determining
+    # if any variables have been set by the user
+    specified = [arg for arg in sys.argv if arg.startswith('-')]
+    nonverbose = [opt for opt in specified if '-v' not in opt]
+    if len(nonverbose) == 0:
+        parser.print_help()
+        sys.exit(1)
 
-# parse arguments and assign values
-args = parser.parse_args()
-DIRECTORIES = args.directory
-XMLREGEX = args.xmlregex
-PATENTROOT = args.patentroot
-# adjust verbosity levels based on specified input
-logging_levels = {0: logging.ERROR,
-                  1: logging.WARNING,
-                  2: logging.INFO,
-                  3: logging.DEBUG}
-VERBOSITY = logging_levels[args.verbosity]
+    # parse arguments and assign values
+    args = parser.parse_args()
+    DIRECTORIES = args.directory
+    XMLREGEX = args.xmlregex
+    PATENTROOT = args.patentroot
+    # adjust verbosity levels based on specified input
+    logging_levels = {0: logging.ERROR,
+                      1: logging.WARNING,
+                      2: logging.INFO,
+                      3: logging.DEBUG}
+    VERBOSITY = logging_levels[args.verbosity]
 
-logfile = "./" + 'xml-parsing.log'
-logging.basicConfig(filename=logfile, level=VERBOSITY)
+    logfile = "./" + 'xml-parsing.log'
+    logging.basicConfig(filename=logfile, level=VERBOSITY)
 
-t1 = datetime.datetime.now()
+    t1 = datetime.datetime.now()
 
-#get a listing of all files within the directory that follow the naming pattern
-files = list_files(DIRECTORIES, PATENTROOT, XMLREGEX)
-logging.info("Total files: %d" % (len(files)))
+    #get a listing of all files within the directory that follow the naming pattern
+    files = list_files(DIRECTORIES, PATENTROOT, XMLREGEX)
+    logging.info("Total files: %d" % (len(files)))
 
-# list of parsed xml strings
-parsed_xmls = parallel_parse(files)
+    # list of parsed xml strings
+    parsed_xmls = parallel_parse(files)
 
-xmlclasses = [AssigneeXML, CitationXML, ClassXML, InventorXML, \
-              PatentXML, PatdescXML, LawyerXML, ScirefXML, UsreldocXML]
+    xmlclasses = [AssigneeXML, CitationXML, ClassXML, InventorXML, \
+                  PatentXML, PatdescXML, LawyerXML, ScirefXML, UsreldocXML]
 
-total_count = 0
-total_patents = 0
+    total_count = 0
+    total_patents = 0
 
-for filename in parsed_xmls:
+    for us_patent_grant in parsed_xmls[0]:
 
-    xmllist = []
-    count = 0
-    patents = 0
+        xmllist = []
+        count = 0
+        patents = 0
 
-    for i, x in enumerate(parsed_xmls):
         for xmlclass in xmlclasses:
             try:
-                xmllist.append(xmlclass(x))
+                xmllist.append(xmlclass(us_patent_grant))
                 patents += 1
             except Exception as inst:
                 logging.error(type(inst))
-                logging.error("  - Error: %s (%d)  %s" % (filename, i, x[175:200]))
+                logging.error("  - Error: %s (%d)  %s" % (us_patent_grant, i, x[175:200]))
                 count += 1
 
-    logging.info("   - number of patents: %d %s ", len(xmllist), datetime.datetime.now()-t1)
-    logging.info( "   - number of errors: %d", count)
-    total_count += count
-    total_patents += patents
+        logging.info("   - number of patents: %d %s ", len(xmllist), datetime.datetime.now()-t1)
+        logging.info( "   - number of errors: %d", count)
+        total_count += count
+        total_patents += patents
 
-    for patent in xmllist:
-        patent.insert_table()
+        for patent in xmllist:
+            patent.insert_table()
 
-    logging.info("   - %s", datetime.datetime.now()-t1)
-    logging.info("   - total errors: %d", total_count)
-    logging.info("   - total patents: %d", total_patents)
+        logging.info("   - %s", datetime.datetime.now()-t1)
+        logging.info("   - total errors: %d", total_count)
+        logging.info("   - total patents: %d", total_patents)
