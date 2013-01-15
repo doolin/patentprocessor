88ce1b5c46cc0cc7ab3ef47a89a51765dda8d6a9
Dave Doolin <david.doolin@gmail.com>
2013-01-15 15:16:39 -0800

    two patent integration script for parsing running.

--- parse_integration.sh
+++ parse_integration.sh
@@ -13,3 +13,16 @@ do
   sqlite3 ${table}.sqlite3 -csv "select * from ${table}"  > /tmp/integration/ipg120327.one/${table}.csv
   `diff test/integration/parse/ipg120327.one/${table}.csv /tmp/integration/ipg120327.one/${table}.csv`
 done
+
+# TODO: Refactor
+make spotless > /dev/null
+mkdir -p /tmp/integration/ipg120327.two
+./parse.py -d test/fixtures/xml/ -p. -x ipg120327.two.xml
+
+for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
+do
+  sqlite3 ${table}.sqlite3 -csv "select * from ${table}"  > /tmp/integration/ipg120327.two/${table}.csv
+  `diff test/integration/parse/ipg120327.two/${table}.csv /tmp/integration/ipg120327.two/${table}.csv`
+done
+
+
