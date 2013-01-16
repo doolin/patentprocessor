09aad7244a33ae64cee838d5b807c7bc7826f725
Dave Doolin <david.doolin@gmail.com>
2013-01-16 15:22:55 -0800

    refactored a db setup function, needs unit testing.

---
+++ lib/geocode_setup.py
@@ -0,0 +1,30 @@
+# sets up the geocoding databases
+
+# Bring in one function at a time.
+
+# TODO: Find a way to unit test fix_city_country
+def fix_city_country(conn):
+    conn.executescript("""
+        CREATE TEMPORARY TABLE temp AS
+            SELECT  Upper(City) as CityX, Upper(State) as StateX,
+                    Upper(Country) as CountryX, count(*) as Cnt
+              FROM  asg.assignee
+             WHERE  City!=""
+          GROUP BY  CityX, StateX, CountryX;
+        CREATE TEMPORARY TABLE temp2 AS
+            SELECT  sum(Cnt) as Cnt,
+                    cityctry(CityX, CountryX, 'city') as CityY, StateX as StateY,
+                    cityctry(CityX, CountryX, 'ctry') as CtryY, '' as ZipcodeY
+              FROM  temp
+             WHERE  CityY!=""
+          GROUP BY  CityY, StateY, CtryY;
+        INSERT OR REPLACE INTO loc
+            SELECT  a.*, SUBSTR(CityY,1,3), b.NewCity, b.NewState, b.NewCountry
+              FROM  temp2 AS a
+         LEFT JOIN  loc.typos AS b
+                ON  a.CityY=b.City AND a.StateY=b.State AND a.CtryY=b.Country;
+        DROP TABLE  temp;
+        DROP TABLE  temp2;
+        """)
+
+
