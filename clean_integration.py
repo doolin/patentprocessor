cfd38ffdbea7bcac1d60e8d6819f2a31fa57b87b
Dave Doolin <david.doolin@gmail.com>
2013-01-16 09:15:37 -0800

    integration testing for 18 rows clean script

--- clean_integration.py
+++ clean_integration.py
@@ -2,6 +2,8 @@
 
 # Integration testing for the clean.py script
 
+##### Two rows
+
 make spotless > /dev/null
 ./parse.py -d test/fixtures/xml/ -p. -x ipg120327.two.xml
 mkdir -p /tmp/integration/ipg120327.two
@@ -12,7 +14,6 @@ do
   sqlite3 inventor.sqlite3 -csv "select * from ${table}"  > /tmp/integration/ipg120327.two/${table}.csv
   `diff test/integration/clean/ipg120327.two/${table}.csv /tmp/integration/ipg120327.two/${table}.csv`
 done
-
 
 for table in assignee assignee_1 grp wrd
 do
@@ -25,3 +26,31 @@ do
   sqlite3 patent.sqlite3 -csv "select * from ${table}"  > /tmp/integration/ipg120327.two/${table}.csv
   `diff test/integration/clean/ipg120327.two/${table}.csv /tmp/integration/ipg120327.two/${table}.csv`
 done
+
+
+#### 18 rows
+
+make spotless > /dev/null
+./parse.py -d test/fixtures/xml/ -p. -x ipg120327.18.xml
+mkdir -p /tmp/integration/ipg120327.18
+python clean.py
+
+for table in inventor inventor_1
+do
+  sqlite3 inventor.sqlite3 -csv "select * from ${table}"  > /tmp/integration/ipg120327.18/${table}.csv
+  `diff test/integration/clean/ipg120327.18/${table}.csv /tmp/integration/ipg120327.18/${table}.csv`
+done
+
+for table in assignee assignee_1 grp wrd
+do
+  sqlite3 assignee.sqlite3 -csv "select * from ${table}"  > /tmp/integration/ipg120327.18/${table}.csv
+  `diff test/integration/clean/ipg120327.18/${table}.csv /tmp/integration/ipg120327.18/${table}.csv`
+done
+
+for table in patent
+do
+  sqlite3 patent.sqlite3 -csv "select * from ${table}"  > /tmp/integration/ipg120327.18/${table}.csv
+  `diff test/integration/clean/ipg120327.18/${table}.csv /tmp/integration/ipg120327.18/${table}.csv`
+done
+
+
