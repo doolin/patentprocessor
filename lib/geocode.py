b95343a46d9c2dd8a15b4b484333720a10736dab
Dave Doolin <david.doolin@gmail.com>
2013-01-16 11:05:47 -0800

    extended comment on unit testing and design to eliminate

--- lib/geocode.py
+++ lib/geocode.py
@@ -185,7 +185,8 @@ def create_locMerge_table(conn):
 create_locMerge_table(c)
 
 # TODO: Unit test this so that it and the unit test can be
-# eliminated in a future redesign.
+# eliminated in a future redesign. Also, ensure that this
+# is the correct name for this function, and adjust accordingly.
 def table_temp1_has_rows(conn):
     return conn.execute("SELECT count(*) FROM temp1").fetchone()[0] > 0
 
