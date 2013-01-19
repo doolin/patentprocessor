838e90cce2f401871b2e52f7acc12888d4504f7c
Dave Doolin <david.doolin@gmail.com>
2013-01-18 18:33:41 -0800

    cleaned up inventor schema

--- schemas/inventor.sql
+++ schemas/inventor.sql
@@ -1,19 +1,36 @@
-CREATE TABLE gXML ( week VARCHAR );
+/**
+ * TODO: Document appropriately
+ * Patent:
+ * Firstname:
+ * Lastname:
+ * Street:
+ * City:
+ * State:
+ * Country:
+ * Zipcode:
+ * Nationality:
+ * InvSeq:
+ */
+
+-- TODO: Remove quote from "inventor"
+-- TODO: Lower case column names
+-- TODO: Pluralize table name to tables
+-- TODO: Underscore separate InvSeq
 CREATE TABLE "inventor" (
-                Patent VARCHAR(8),      Firstname VARCHAR(15),  Lastname VARCHAR(15),
-                Street VARCHAR(15),     City VARCHAR(10),
-                State VARCHAR(2),       Country VARCHAR(12),
-                Zipcode VARCHAR(5),     Nationality VARCHAR(2), InvSeq INTEGER);
-CREATE TABLE "inventor_1" (
-                Patent VARCHAR(8),      Firstname VARCHAR(15),  Lastname VARCHAR(15),
-                Street VARCHAR(15),     City VARCHAR(10),
-                State VARCHAR(2),       Country VARCHAR(12),
-                Zipcode VARCHAR(5),     Nationality VARCHAR(2), InvSeq INTEGER, NCity, NState, NCountry, NZipcode, NLat, NLong);
-CREATE INDEX idx_cty ON "inventor" (Country);
-CREATE INDEX idx_patent ON "inventor" (Patent);
-CREATE INDEX idx_stt ON "inventor" (State);
-CREATE INDEX inventor_1_idx_cty ON "inventor_1" (Country);
-CREATE INDEX inventor_1_idx_patent ON "inventor_1" (Patent);
-CREATE INDEX inventor_1_idx_stt ON "inventor_1" (State);
-CREATE UNIQUE INDEX inventor_1_uqInv ON "inventor_1" (Patent, InvSeq);
+  Patent      VARCHAR(8),
+  Firstname   VARCHAR(15),
+  Lastname    VARCHAR(15),
+  Street      VARCHAR(15),
+  City        VARCHAR(10),
+  State       VARCHAR(2),
+  Country     VARCHAR(12),
+  Zipcode     VARCHAR(5),
+  Nationality VARCHAR(2),
+  InvSeq      INTEGER
+);
+
+
+CREATE INDEX idx_cty      ON "inventor" (Country);
+CREATE INDEX idx_patent   ON "inventor" (Patent);
+CREATE INDEX idx_stt      ON "inventor" (State);
 CREATE UNIQUE INDEX uqInv ON "inventor" (Patent, InvSeq);
