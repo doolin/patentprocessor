d8de657f950dfd71272b9d66ee4e1744adee990d
Dave Doolin <david.doolin@gmail.com>
2013-01-18 18:28:14 -0800

    cleaned up class schema definition

--- schemas/class.sql
+++ schemas/class.sql
@@ -1,7 +1,22 @@
+/**
+ * TODO: Document appropriately
+ * Patent:
+ * Prim:
+ * Class:
+ * SubClass:
+*/
+
+-- TODO: Rename table to classes
+-- TODO: Lower case column names
+-- TODO: Underscore separate SubClass
 CREATE TABLE class (
-                Patent VARCHAR(8),      Prim INTEGER,
-                Class VARCHAR(3),       SubClass VARCHAR(3));
-CREATE TABLE gXML ( week VARCHAR );
+  Patent   VARCHAR(8),
+  Prim     INTEGER,
+  Class    VARCHAR(3),
+  SubClass VARCHAR(3)
+);
+
+-- TODO: regularize index names
 CREATE INDEX idx_cls  ON class (Class);
 CREATE INDEX idx_cscl ON class (Class,SubClass);
 CREATE INDEX idx_patent ON class (Patent);
