ebbe4112bda2b3463b8cfb490886e10a31f9f1b3
Dave Doolin <david.doolin@gmail.com>
2013-01-19 12:50:02 -0800

    cleaned lawyer schema

--- schemas/lawyer.sql
+++ schemas/lawyer.sql
@@ -1,7 +1,26 @@
-CREATE TABLE gXML ( week VARCHAR );
+/**
+ * TODO: Document appropriately
+ * Patent:
+ * Firstname:
+ * Lastname:
+ * LawCountry:
+ * OrgName:
+ * LawSeq:
+ */
+
+-- TODO: Pluralize table name
+-- TODO: Lower case column names
+-- TODO: Underscore separate appropriately
 CREATE TABLE lawyer (
-                Patent VARCHAR(8),      Firstname VARCHAR(15),  Lastname VARCHAR(15),
-                LawCountry VARCHAR(2),  OrgName VARCHAR(20),    LawSeq INTEGER);
-CREATE INDEX idx_patent ON lawyer (Patent);
-CREATE INDEX idx_patl ON lawyer (Patent, LawSeq);
+  Patent     VARCHAR(8),
+  Firstname  VARCHAR(15),
+  Lastname   VARCHAR(15),
+  LawCountry VARCHAR(2),
+  OrgName    VARCHAR(20),
+  LawSeq     INTEGER
+);
+
+-- TODO: Regularize index names
+CREATE INDEX idx_patent      ON lawyer (Patent);
+CREATE INDEX idx_patl        ON lawyer (Patent, LawSeq);
 CREATE UNIQUE INDEX uqLawyer ON lawyer (Patent, LawSeq);
