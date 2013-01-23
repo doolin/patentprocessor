e7ccb11a9c4ff874db334d0f05ebb5f3d29f65c3
Gabe Fierro <gtfierro225@gmail.com>
2013-01-23 14:09:54 -0800

    add argconfig, parse files as basis for automated db insertion

---
+++ couch_patent/argconfig_parse.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+import logging
+
+class ArgHandler(object):
+    
+    def __init__(self, arglist):
+        self.arglist = arglist
+
+        # setup argparse
+        self.parser = argparse.ArgumentParser(description=\
+                'Specify source directory/directories for xml files to be parsed')
+        self.parser.add_argument('--directory','-d', type=str, nargs='+', default='.',
+                help='comma separated list of directories relative to $PATENTROOT that \
+                parse.py will search for .xml files')
+        self.parser.add_argument('--patentroot','-p', type=str, nargs='?',
+                default=os.environ['PATENTROOT'] \
+                if os.environ.has_key('PATENTROOT') else '/',
+                help='root directory of all patent files/directories')
+        self.parser.add_argument('--xmlregex','-x', type=str, 
+                nargs='?', default=r"ipg\d{6}.xml",
+                help='regex used to match xml files in each directory')
+        self.parser.add_argument('--verbosity', '-v', type = int,
+                nargs='?', default=0,
+                help='Set the level of verbosity for the computation. The higher the \
+                verbosity level, the less restrictive the print policy. 0 (default) \
+                = error, 1 = warning, 2 = info, 3 = debug')
+
+        # parse arguments and assign values
+        args = self.parser.parse_args(self.arglist)
+        self.directories = args.directory
+        self.xmlregex = args.xmlregex
+        self.patentroot = args.patentroot
+
+        # adjust verbosity levels based on specified input
+        logging_levels = {0: logging.ERROR,
+                          1: logging.WARNING,
+                          2: logging.INFO,
+                          3: logging.DEBUG}
+        self.verbosity = logging_levels[args.verbosity]
+
+    def invalid_config(self):
+        # double check that variables are actually set
+        # we ignore the verbosity argument when determining
+        # if any variables have been set by the user
+        specified = [arg for arg in self.arglist if arg.startswith('-')]
+        nonverbose = [opt for opt in specified if '-v' not in opt]
+        return len(nonverbose) == 0
+        
+    def get_directory_list(self):
+        return self.directories
+
+    def get_xmlregex(self):
+        return self.xmlregex
+
+    def get_patentroot(self):
+        return self.patentroot
+
+    def get_verbosity(self):
+        return self.verbosity
+
+    def get_help(self):
+        self.parser.print_help()
+        sys.exit(1)
---
+++ couch_patent/parse.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import logging
+# http://docs.python.org/howto/argparse.html
+import argparse
+import os
+import datetime
+import re
+import mmap
+import contextlib
+import multiprocessing
+import itertools
+
+import sys
+sys.path.append( '.' )
+sys.path.append( './lib/' )
+
+from patXML import *
+from argconfig_parse import ArgHandler
+
+regex = re.compile(r"""
+ ([<][?]xml[ ]version.*?[>]       #all XML starts with ?xml
+.*?
+[<][/]us[-]patent[-]grant[>])    #and here is the end tag
+""", re.I+re.S+re.X)
+
+# TODO: put into configuration file (low priority)
+xmlclasses = [AssigneeXML, CitationXML, ClassXML, InventorXML, \
+              PatentXML, PatdescXML, LawyerXML, ScirefXML, UsreldocXML]
+
+
+def list_files(directories, patentroot, xmlregex):
+    """
+    Returns listing of all files within all directories relative to patentroot
+    whose filenames match xmlregex
+    """
+    files = [patentroot+'/'+directory+'/'+fi for directory in directories for fi in \
+            os.listdir(patentroot+'/'+directory) \
+            if re.search(xmlregex, fi, re.I) != None]
+    if not files:
+        logging.error("No files matching {0} found in {1}/{2}".format(XMLREGEX,PATENTROOT,DIRECTORIES))
+        sys.exit(1)
+    return files
+
+def parse_file(filename):
+    parsed_xmls = []
+    size = os.stat(filename).st_size
+    with open(filename,'r') as f:
+        with contextlib.closing(mmap.mmap(f.fileno(), size, access=mmap.ACCESS_READ)) as m:
+            parsed_xmls.extend(regex.findall(m))
+    return parsed_xmls
+
+def parallel_parse(filelist):
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    parsed = pool.imap_unordered(parse_file, filelist)
+    return list(itertools.chain.from_iterable(parsed))
+
+def parse_patent(grant_list):
+    parsed_grants = []
+    for us_patent_grant in grant_list:
+        for xmlclass in xmlclasses:
+            try:
+                parsed_grants.append(xmlclass(us_patent_grant))
+            except Exception as inst:
+                logging.error(type(inst))
+                logging.error("  - Error: %s %s" % (us_patent_grant, us_patent_grant[175:200]))
+    return parsed_grants
+
+# TODO: unittest
+def build_tables(parsed_grants):
+    for us_patent_grant in parsed_grants:
+        us_patent_grant.insert_table()
+
+if __name__ == '__main__':
+
+    args = ArgHandler(sys.argv[1:])
+
+    if args.invalid_config():
+        args.get_help()
+
+    DIRECTORIES = args.get_directory_list()
+    XMLREGEX = args.get_xmlregex()
+    PATENTROOT = args.get_patentroot()
+    VERBOSITY = args.get_verbosity()
+
+    logfile = "./" + 'xml-parsing.log'
+    logging.basicConfig(filename=logfile, level=VERBOSITY)
+
+    t1 = datetime.datetime.now()
+
+    files = list_files(DIRECTORIES, PATENTROOT, XMLREGEX)
+    parsed_xmls = parallel_parse(files)
+    parsed_grants = parse_patent(parsed_xmls)
+    build_tables(parsed_grants)
+
+    total_patents = len(parsed_xmls)
+    total_errors = len(parsed_xmls) * len(xmlclasses) - len(parsed_grants)
+
+    logging.info("Parsing started at %s", str(datetime.datetime.today()))
+    logging.info("Time Elapsed: %s", datetime.datetime.now()-t1)
+    logging.info("Total Patent Files: %d" % (len(files)))
+    logging.info("Total Errors: %d", total_errors)
+    logging.info("Total Patents: %d", total_patents)
