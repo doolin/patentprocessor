ba01ebf7b2ec714d5e1db3212c55af29fce9167e
Dave Doolin <david.doolin@gmail.com>
2013-01-23 15:33:52 -0800

    patent schema cleanup

--- schemas/patent.sql
+++ schemas/patent.sql
@@ -1,10 +1,37 @@
-CREATE TABLE gXML ( week VARCHAR );
+/**
+ * TODO: Document appropriately.
+ * Patent  VARCHAR(8),
+ * Kind    VARCHAR(3),
+ * Claims  INTEGER,
+ * AppType INTEGER,
+ * AppNum  VARCHAR(8),
+ * GDate   INTEGER,
+ * GYear   INTEGER,
+ * AppDate INTEGER,
+ * AppYear INTEGER,
+ * PatType VARCHAR(15)
+ */
+
+-- TODO: Pluralize table name
+-- TODO: Lowercase column names
+-- TODO: Underscore appropriately
 CREATE TABLE patent (
-                Patent VARCHAR(8),      Kind VARCHAR(3),        Claims INTEGER,
-                AppType INTEGER,        AppNum VARCHAR(8),
-                GDate INTEGER,          GYear INTEGER,
-                AppDate INTEGER,        AppYear INTEGER, PatType VARCHAR(15) );
-CREATE INDEX idx_ayr ON patent (AppYear);
-CREATE INDEX idx_gyr ON patent (GYear);
-CREATE INDEX idx_patent ON patent (Patent);
-CREATE UNIQUE INDEX uqPat on patent (Patent);
+  Patent VARCHAR(8),
+  Kind VARCHAR(3),
+  Claims INTEGER,
+  AppType INTEGER,
+  AppNum VARCHAR(8),
+  GDate INTEGER,
+  GYear INTEGER,
+  AppDate INTEGER,
+  AppYear INTEGER,
+  PatType VARCHAR(15)
+);
+
+
+CREATE INDEX idx_ayr      ON patent (AppYear);
+CREATE INDEX idx_gyr      ON patent (GYear);
+CREATE INDEX idx_patent   ON patent (Patent);
+CREATE UNIQUE INDEX uqPat ON patent (Patent);
+
+-- TODO: Add example XML snippet
