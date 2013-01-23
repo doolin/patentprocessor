e66576ffc0fe8e08004d92c89a9cad9ee97f877f
Dave Doolin <david.doolin@gmail.com>
2013-01-23 15:28:10 -0800

    cleaned up patdesc shcema

--- schemas/patdesc.sql
+++ schemas/patdesc.sql
@@ -1,6 +1,20 @@
-CREATE TABLE gXML ( week VARCHAR );
+/**
+ * TODO: Document appropriately
+ * Patent   VARCHAR(8),
+ * Abstract VARCHAR(50),
+ * Title    VARCHAR(20)
+ */
+
+-- TODO: Pluralize table name
+-- TODO: Lowercase columns
 CREATE TABLE patdesc (
-                Patent VARCHAR(8),
-                Abstract VARCHAR(50),   Title VARCHAR(20));
+  Patent   VARCHAR(8),
+  Abstract VARCHAR(50),
+  Title    VARCHAR(20)
+);
+
+-- TODO: rename indexes appropriately.
 CREATE INDEX idx_patent ON patdesc (Patent);
 CREATE UNIQUE INDEX uqPatDesc ON patdesc (Patent);
+
+-- TODO: Add example XML snippet
