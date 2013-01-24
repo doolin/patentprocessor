d4e8d085c8d6a4a4347b2f95eed8e56e56e7fe75
Dave Doolin <david.doolin@gmail.com>
2013-01-23 16:33:20 -0800

    cleaned up usreldoc schema

--- schemas/usreldoc.sql
+++ schemas/usreldoc.sql
@@ -1,8 +1,32 @@
-CREATE TABLE gXML ( week VARCHAR );
+/**
+ * TODO: Document appropriately.
+ * Patent    VARCHAR(8),
+ * DocType   VARCHAR(10),
+ * OrderSeq  INTEGER,
+ * Country   VARCHAR(2),
+ * RelPatent VARCHAR(8),
+ * Kind      VARCHAR(2),
+ * RelDate   INTEGER,
+ * Status    VARCHAR(10)
+ */
+
+-- TODO: Pluralize table name
+-- TODO: Underscore appropriately.
+-- TODO: Lowercase column names.
 CREATE TABLE usreldoc (
-                Patent VARCHAR(8),      DocType VARCHAR(10),    OrderSeq INTEGER,
-                Country VARCHAR(2),     RelPatent VARCHAR(8),   Kind VARCHAR(2),
-                RelDate INTEGER,        Status VARCHAR(10));
-CREATE INDEX idx_patent ON usreldoc (Patent);
-CREATE INDEX idx_relpat ON usreldoc (RelPatent);
+  Patent    VARCHAR(8),
+  DocType   VARCHAR(10),
+  OrderSeq  INTEGER,
+  Country   VARCHAR(2),
+  RelPatent VARCHAR(8),
+  Kind      VARCHAR(2),
+  RelDate   INTEGER,
+  Status    VARCHAR(10)
+);
+
+-- TODO: Rename indexes appropriately.
+CREATE INDEX idx_patent        ON usreldoc (Patent);
+CREATE INDEX idx_relpat        ON usreldoc (RelPatent);
 CREATE UNIQUE INDEX uqUSRelDoc ON usreldoc (Patent, OrderSeq);
+
+-- TODO: Add example XML snippet
