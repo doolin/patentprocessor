59b4dfade37a7ff7ccee3c856764766b5222fc0e
Gabe Fierro <gtfierro225@gmail.com>
2013-01-24 14:34:55 -0800

    establish test file

--- couch_patent/couch_patent.py
+++ couch_patent/couch_patent.py
@@ -7,6 +7,10 @@ import unittest
 sys.path.append('../lib')
 from patXML import *
 
+basedir = os.path.join(os.curdir, '../test')
+testdir = os.path.join(basedir, 'fixtures/xml/')
+
+
 class TestCouchPatent(unittest.TestCase):
 
     def setUp(self):
@@ -17,4 +21,7 @@ class TestCouchPatent(unittest.TestCase):
         else:
             self.db = self.couch.create('patents')
         self.assertTrue('patents' in self.couch)
+        #establish test xml file
+        self.testfile = XMLPatentBase(open(testdir+'ipg120327.one.xml').read())
+        self.assertTrue(self.testfile)
 
