bc0c4b35cb8320d794a2fda64a103a0c52e2f32d
Dave Doolin <david.doolin@gmail.com>
2013-01-23 16:00:38 -0800

    cleaned up sciref sceham

--- schemas/sciref.sql
+++ schemas/sciref.sql
@@ -1,5 +1,23 @@
-CREATE TABLE gXML ( week VARCHAR );
+/**
+ * TODO: Document appropriately
+ * Patent  VARCHAR(8),
+ * Descrip VARCHAR(20),
+ * CitSeq  INTEGER
+ */
+
+
+-- TODO: Pluralize table name
+-- TODO: Underscore appropriately
+-- TODO: Lowercase column names
 CREATE TABLE sciref (
-                Patent VARCHAR(8),      Descrip VARCHAR(20),    CitSeq INTEGER);
-CREATE INDEX idx_patent ON sciref (Patent);
+  Patent  VARCHAR(8),
+  Descrip VARCHAR(20),
+  CitSeq  INTEGER
+);
+
+
+-- TODO: rename indexes appropriately.
+CREATE INDEX idx_patent      ON sciref (Patent);
 CREATE UNIQUE INDEX uqSciref ON sciref (Patent, CitSeq);
+
+-- TODO: Add example XML snippet
