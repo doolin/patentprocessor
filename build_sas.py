5e7bb6df9f81f13c8bf69f399acaf73364991972
Dave Doolin <david.doolin@gmail.com>
2013-01-27 18:07:51 -0800

    cleaned up schema formatting

--- build_sas.py
+++ build_sas.py
@@ -7,18 +7,23 @@ from fwork import *
 from config import *
 
 # TODO: Find a file that represents US Cities and Zipcodes
-#       for now, use CD_ZIP.sqlite3 
+#       for now, use CD_ZIP.sqlite3
 
 s = SQLite.SQLite(loc["db"], tbl=loc["nat_tbl"])
 s.attach(loc["us_db"])
 s.replicate(tbl=loc["us_tbl"], db="db")
 s.addSQL(loc["us_tbl"], db="db", errlog=loc["nat_error"], header=True)
-s.c.executescript(""" 
+
+s.c.executescript("""
     CREATE TABLE IF NOT EXISTS typos (
-        City VARCHAR,       State VARCHAR,      Country VARCHAR,
-        NewCity VARCHAR,    NewState VARCHAR,   NewCountry VARCHAR
-        );
-    """) #"""
+        City       VARCHAR,
+        State      VARCHAR,
+        Country    VARCHAR,
+        NewCity    VARCHAR,
+        NewState   VARCHAR,
+        NewCountry VARCHAR);
+    """)
+
 s.chgTbl("typos")
 s.index(["City", "State", "Country"], unique=True)
 s.index(["Country"])
