31b35ca74a41638a903ac50640a4b5131f57e6e7
Gabe Fierro <gtfierro225@gmail.com>
2013-01-29 10:23:48 -0800

    Revert "speedup parsing using map and adjusting poolsize"
    This reverts commit 8e048379cc298123a7ff19c60f582abde6bdbd07.

--- parse.py
+++ parse.py
@@ -53,9 +53,8 @@ def parse_file(filename):
 
 def parallel_parse(filelist):
     if not filelist: return
-    poolsize = min( max(len(filelist),1), multiprocessing.cpu_count() )
-    pool = multiprocessing.Pool(poolsize)
-    parsed = pool.map(parse_file, filelist)
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    parsed = pool.imap_unordered(parse_file, filelist)
     return list(itertools.chain.from_iterable(parsed))
 
 def parse_patent(grant_list):
