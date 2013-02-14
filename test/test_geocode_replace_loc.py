6527110c73b5d0f07f7aacffe7d3232ef0661247
Dave Doolin <david.doolin@gmail.com>
2013-02-14 11:54:34 -0800

    first crack at setting up geocode_replace_loc testing

--- test/test_geocode_replace_loc.py
+++ test/test_geocode_replace_loc.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 
-import unittest
 import sys
+import sqlite3
+import unittest
+
 sys.path.append( '.' )
 sys.path.append( '../lib/' )
 
-#import geocode
+from make_test_databases import *
+from geocode_setup import create_hashtbl
+from geocode_setup import create_sql_helper_functions
 
 
 class TestGeocode(unittest.TestCase):
 
-    def setUp(self):
-        self.foo = 'bar'
+    @classmethod
+    def setUpClass(self):
+        remove_existing_databases()
+        make_assignee_db()
+        make_inventor_db()
+        self.conn = sqlite3.connect("hashTbl.sqlite3")
+        cursor = self.conn.cursor()
+        create_sql_helper_functions(self.conn)
+        create_hashtbl(cursor, self.conn)
+
+    def test_domestic_sql(self):
+        pass
 
 
 if __name__ == '__main__':
