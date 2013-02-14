d6b97bc8c3d46482518328a23cd40e1db2299abb
Dave Doolin <david.doolin@gmail.com>
2013-02-14 15:03:31 -0800

    added stubs for replace_loc unit tests

--- test/test_geocode_replace_loc.py
+++ test/test_geocode_replace_loc.py
@@ -12,7 +12,7 @@ from geocode_setup import create_hashtbl
 from geocode_setup import create_sql_helper_functions
 from geocode_replace_loc import *
 
-class TestGeocode(unittest.TestCase):
+class TestGeocodeReplaceLoc(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
@@ -26,9 +26,109 @@ class TestGeocode(unittest.TestCase):
 
     def test_domestic_sql(self):
         query = domestic_sql() % (-1, -1)
-        #print query
         result = self.cursor.execute(query)
-        #print result
+        rows = result.fetchall()
+        #print rows
+        query = domestic_sql() % (0, 0)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        #print rows
+        pass
+
+    def test_domestic_block_remove_sql(self):
+        query = domestic_block_remove_sql() % (-1, -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_domestic_first3_jaro_winkler_sql(self):
+        query = domestic_first3_jaro_winkler_sql() % (-1, -1, "10.92", -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_domestic_last4_jaro_winkler_sql(self):
+        query = domestic_last4_jaro_winkler_sql() % (-1, -1, "10.90", -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_full_name_1_sql(self):
+        query = foreign_full_name_1_sql() % (-1, -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_full_name_2_sql(self):
+        query = foreign_full_name_2_sql() % (-1, -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_short_form_sql(self):
+        query = foreign_short_form_sql() % (-1, -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_block_split_sql(self):
+        query = foreign_block_split_sql() % (-1, -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_first3_jaro_winkler_sql(self):
+        query = foreign_first3_jaro_winkler_sql() % (-1, -1, "20.92", -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_last4_jaro_winkler_sql(self):
+        query = foreign_last4_jaro_winkler_sql() % (-1, -1, "20.90", -1)
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_domestic_2nd_layer_sql(self):
+        query = domestic_2nd_layer_sql()
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_domestic_first3_2nd_jaro_winkler_sql(self):
+        query = domestic_first3_2nd_jaro_winkler_sql() % "14.95"
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_full_name_2nd_layer_sql(self):
+        query = foreign_full_name_2nd_layer_sql()
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_full_nd_2nd_layer_sql(self):
+        query = foreign_full_nd_2nd_layer_sql()
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_no_space_2nd_layer_sql(self):
+        query = foreign_no_space_2nd_layer_sql()
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_foreign_first3_2nd_jaro_winkler_sql(self):
+        query = foreign_first3_2nd_jaro_winkler_sql() % "24.95"
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
+        pass
+
+    def test_domestic_zipcode_sql(self):
+        query = domestic_zipcode_sql()
+        result = self.cursor.execute(query)
+        rows = result.fetchall()
         pass
 
 
