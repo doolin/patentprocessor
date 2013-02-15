b721f1ebd92e9916e47062a0dc31c1b3ca8227d4
Dave Doolin <david.doolin@gmail.com>
2013-02-15 14:46:34 -0800

    added more patents to parse integration testing

--- parse_integration.sh
+++ parse_integration.sh
@@ -25,4 +25,14 @@ do
   diff test/integration/parse/ipg120327.two/${table}.csv /tmp/integration/ipg120327.two/${table}.csv
 done
 
+make spotless > /dev/null
+mkdir -p /tmp/integration/ipg120327.18
+./parse.py -d test/fixtures/xml/ -p. -x ipg120327.18.xml
+
+for table in assignee citation class inventor lawyer patdesc patent sciref usreldoc
+do
+  sqlite3 -csv ${table}.sqlite3 "select * from ${table}"  > /tmp/integration/ipg120327.18/${table}.csv
+  diff test/integration/parse/ipg120327.18/${table}.csv /tmp/integration/ipg120327.18/${table}.csv
+done
+
 
